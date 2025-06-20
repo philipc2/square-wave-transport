9d35df1fd3a40cee9e2fc7d7ea601b0d5311fa8a
Philip Chmielowiec <philipchmielowiec@gmail.com>
2025-06-20 14:58:28 -0500

    update cmake

--- src/main.c
+++ src/main.c
@@ -1,4 +1,23 @@
-#include "flux_corr_method.h"
 
-int main() { return 0; }
-x
+#include "flux_corr_method.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+  // Configuration
+  size_t N = 1000;
+  double dt = 0.1;
+  double *U = malloc(N * sizeof(*U));
+  double *V = malloc(N * sizeof(*V));
+  double *Unew = malloc(N * sizeof(*Unew));
+
+  flux_corr_method(U, V, N, dt, dx, Unew);
+
+  // TODO: Save output
+
+  free(U);
+  free(V);
+  free(Unew);
+  return 0;
+}
